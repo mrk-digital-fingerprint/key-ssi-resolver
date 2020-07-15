4c93446ee4ddb495dcc630d0fe07b680c009d9da
skutner <sava.dumitru.daniel@gmail.com>
2020-07-15 11:01:30 +0300

    added PasswordSSI

---
+++ tests/SSIs/getZaSSIFromPasswordSSI.js
@@ -0,0 +1,12 @@
+require("../../../../psknode/bundles/testsRuntime");
+const crypto = require("pskcrypto");
+const assert = require("double-check").assert;
+const PasswordSSI = require("../../lib/SSIs/SeedSSIs/PasswordSSI");
+
+const passwordSSI = new PasswordSSI()
+passwordSSI.initialize("seed", "domain", "encKey", "control");
+const zaSSIIdentifier = `ssi:za:domain:${crypto.pskBase58Encode(crypto.pskHash(passwordSSI.getEncryptionKey()))}:control:v0`;
+
+assert.begin();
+assert.true(zaSSIIdentifier === passwordSSI.derive().getIdentifier());
+assert.end();
